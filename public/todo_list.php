cc909fa6a434164f9c08d8ec4fd407fb6c4e3cbc
Alex Zuniga <guest@Alexs-MBA.local>
2014-05-29 15:25:27 -0500

    allowing  input to be added to list array. Saved list in order to allow conituous adding of items to array.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,36 +1,54 @@
 <!DOCTYPE html>
 <html>
+<head>
 <?php 
-	
+	function save_file($data_to_save, $filename = 'data/list.txt')
+	{
+    	$handle = fopen($filename, 'w');
+    	$contents = implode("\n", $data_to_save);
+    	fwrite($handle, $contents);
+    	fclose($handle);
+	}
+	///////
+	function import($filename = 'data/list.txt'){
+		if (is_readable($filename) && (filesize($filename) > 0)) {
+			    $handle = fopen($filename, "r");
+			    $contents = fread($handle, filesize($filename));
+			    $file_array = explode("\n", $contents);
+			    fclose($handle);
+			    return $file_array;
+			}
+		}
+///////////	
 	var_dump($_GET);
 	var_dump($_POST);
 ?>
-<head>
 	<title>TODO List</title>
 </head>
 <body>
 		<h1>TODO List</h1>
+			<p>
 			<?php
 			
-			$TODO_list = ['Take out the paper', 'Take out Trash', 'Get no spending cash'];
+			$TODO_list = import();
 
-			foreach($TODO_list as $key => $todo) {
-				echo  "<ul><li>$todo<br></li></ul>";
+			if (!empty($_POST['new_item'])) {
+				$TODO_list[] = $_POST['new_item'];
+				save_file($TODO_list);
 			}
-			// <ul>
-			// 	<li>Take out the paper</li>
-			// 	<li>Take out the trash</li>
-			// 	<li>Get no spending cash</li>
-			// </ul>
 
+			foreach ($TODO_list as $key => $item) {
+				echo "<ul><li>$item<br></li></ul>";
+			}
 			?>
-		<form method="POST">
+			</p>
+		<form method = "POST"> 
 		<p>
-		<label name="list_input"> Items: </label>
-    	<input id="list_input" name="list_input" type="text" placeholder="Place Items Here"></label>
+    		<label> New Item: <input id="new_item" name="new_item" type="text" placeholder="New Item Here" ></label>
     	</p>
-    	<p>
-    		<input type="submit" value="Add to List"></input>
+		<button type="submit"> Enter </button>
 		</form>
+
+
 </body>
 </html>
