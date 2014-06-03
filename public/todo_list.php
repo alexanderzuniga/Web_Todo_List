4105d10a2600dbe8656bea65994c7c835a803fcc
Alex Zuniga <guest@Alexs-MBA.local>
2014-06-03 09:52:17 -0500

    Added list forms. If fields are left emtpy, will echo error  messages. All except phone are needed.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,62 +1,61 @@
 <?
-
 var_dump($_POST);
+var_dump($_GET);
 $filename = 'data/list.txt';
 $TODO_list = import($filename);
 $errorMessage = '';
 //Import a file
-		function import($filename){
-			$items = [];
-			if (is_readable($filename) && (filesize($filename) > 0)) {
-				$handle = fopen($filename, "r");
-				$contents = fread($handle, filesize($filename));
-				$file_array = explode("\n", $contents);
-				fclose($handle);
-				return $file_array;
-			}
-			return $items;
-		}	
-//Saving the File
-	function save_file($data_to_save, $array)
-	{
-		$filename = $data_to_save; 
-		if (is_writable($data_to_save)) {
-			$handle = fopen($filename, 'w');
-			foreach($array as $items){
-				fwrite($handle, PHP_EOL . $items);
-			}
-			fclose($handle);
-		}
+function import($filename){
+	$items = [];
+	if (is_readable($filename) && (filesize($filename) > 0)) {
+		$handle = fopen($filename, "r");
+		$contents = trim(fread($handle, filesize($filename)));
+		$file_array = explode("\n", $contents);
+		fclose($handle);
+		return $file_array;
 	}
-	//Removing items
-		if (isset($_GET['removeIndex'])) {
-			$removeIndex = $_GET['removeIndex'];
-			unset($TODO_list[$removeIndex]);
-			save_file($filename, $TODO_list);
-			header('Location: /todo_list.php');
-		} 
-	// Adding items 
-		if (!empty($_POST['new_item'])) {
-			array_push($TODO_list, $_POST['new_item']);
-			save_file($filename, $TODO_list);
-			header('Location: /todo_list.php');
+	return $items;
+}	
+//Saving the File
+function save_file($data_to_save, $array){
+	$filename = $data_to_save; 
+	if (is_writable($data_to_save)) {
+		$handle = fopen($filename, 'w');
+		foreach($array as $items){
+			fwrite($handle, PHP_EOL . $items);
 		}
+		fclose($handle);
+	}
+}
+	//Removing items
+if (isset($_GET['removeIndex'])) {
+	$removeIndex = $_GET['removeIndex'];
+	unset($TODO_list[$removeIndex]);
+	save_file($filename, $TODO_list);
+	header('Location: /todo_list.php');
+} 
+	// Adding items 
+if (!empty($_POST['new_item'])) {
+	array_push($TODO_list, $_POST['new_item']);
+	save_file($filename, $TODO_list);
+	header('Location: /todo_list.php');
+}
 	//uploading with no errors
-		if (count($_FILES) > 0 && $_FILES['file1']['type'] != "text/plain"){   //File must be text
-			$errorMessage = "Error! File must be plain text. \n";
-			echo $errorMessage;
-		}	else if  (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
-			$Appending_array = ($_FILES);
-			$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
-			$filename = basename($_FILES['file1']['name']);
-			$saved_filename = $upload_dir . $filename;
-			move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+if (count($_FILES) > 0 && $_FILES['file1']['type'] != "text/plain"){   //File must be text
+	$errorMessage = "Error! File must be plain text. \n";
+		echo $errorMessage;
+	} else if  (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+		$Appending_array = ($_FILES);
+		$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+		$filename = basename($_FILES['file1']['name']);
+		$saved_filename = $upload_dir . $filename;
+		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 
 			$testFile = $saved_filename;
 			$newfile= import($testFile);
-			$TODO_list= array_merge($newfile, $TODO_list);
-			save_file($filename, $TODO_list);
-		} 
+			$TODO_list= array_merge($TODO_list, $newfile);
+			save_file($TODO_list, $filename);
+	} 
 ?>
 <!-- INPUT NEW ITEMS -->
 <!DOCTYPE html>
@@ -70,18 +69,22 @@ $errorMessage = '';
 <? endif; ?>
 <ul>
 <? foreach ($TODO_list as $key => $item) : ?>
-<li> <?= htmlspecialchars(strip_tags($item)); ?> <a href= \"todo_list.php?removeIndex=$key\">Remove Item</a><br></li>
+<li> 
+	<?=htmlspecialchars(strip_tags($item));?> 
+	<a href="todo_list.php?removeIndex=<?=$key?>">Remove Item</a><br>
+</li>
 <? endforeach; ?>
 </ul>
+	
+</p>
+<form method = "POST" action="/todo_list.php"> 
+	<p>
+		<label> New Item: <input id="new_item" name="new_item" type="text" placeholder="New Item Here" autofocus></label>
 	</p>
-	<form method = "POST" action="/todo_list.php"> 
-		<p>
-			<label> New Item: <input id="new_item" name="new_item" type="text" placeholder="New Item Here" autofocus></label>
-		</p>
-		<button type="submit"> Enter </button>
-	</form>
-	<h2>Upload File</h2>
-	<hr>
+	<button type="submit"> Enter </button>
+</form>
+<h2>Upload File</h2>
+<hr>
 
 <? if (isset($saved_filename)) : ?>
 <?=  "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>"; ?>
